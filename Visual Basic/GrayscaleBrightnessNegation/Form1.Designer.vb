802db517593e7e9677a02c920e6d8a15225210bd
yogamardia <yoga.mardia@gmail.com>
2018-03-15 22:54:17 +0800

    Add Visual Basic program

---
+++ Visual Basic/GrayscaleBrightnessNegation/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(33, 69)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(250, 250)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox2.Location = New System.Drawing.Point(319, 69)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(250, 250)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(109, 336)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(80, 24)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Open Image"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(104, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 25)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Original"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(683, 41)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 25)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Binary"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox3.Location = New System.Drawing.Point(601, 68)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(258, 250)
+        Me.PictureBox3.TabIndex = 13
+        Me.PictureBox3.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(392, 41)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(118, 25)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Grayscale"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(397, 336)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "Grayscale"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(688, 336)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 18
+        Me.Button3.Text = "Binary"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(784, 420)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 19
+        Me.Button4.Text = "Exit"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(874, 454)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Image Enhancement"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Visual Basic/GrayscaleBrightnessNegation/Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Graphics
+Imports System.Drawing.Imaging
+Public Class Form1
+
+    Private Sub Open_File(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim ofd As New OpenFileDialog
+        ofd.InitialDirectory = "c:\"
+        ofd.Filter = "Image files (*.bmp, *.jpg)|*.bmp;*.jpg;*.png"
+        If ofd.ShowDialog() = DialogResult.OK Then
+            PictureBox1.Image = New Bitmap(ofd.FileName)
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+            PictureBox2.Image = Nothing
+            PictureBox3.Image = Nothing
+        End If
+    End Sub
+
+    Private Sub Grayscale(sender As Object, e As EventArgs) Handles Button2.Click
+        If PictureBox1.Image Is Nothing Then
+            MessageBox.Show("Please open the image first!", "Warning",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Dim source As New Bitmap(PictureBox1.Image)
+            Dim red, green, blue, gray As Integer
+            For y As Integer = 0 To source.Height - 1
+                For x As Integer = 0 To source.Width - 1
+                    red = source.GetPixel(x, y).R
+                    green = source.GetPixel(x, y).G
+                    blue = source.GetPixel(x, y).B
+                    gray = (red + green + blue) / 3
+                    source.SetPixel(x, y, Color.FromArgb(gray, gray, gray))
+                Next
+            Next
+            PictureBox2.Image = source
+            PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+    End Sub
+
+    Private Sub Binary(sender As Object, e As EventArgs) Handles Button3.Click
+        If PictureBox2.Image Is Nothing Then
+            MessageBox.Show("Please grayscale the image first!", "Warning",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Dim source As New Bitmap(PictureBox2.Image)
+            Dim red, green, blue, rgb As Integer
+            For y As Integer = 0 To source.Height - 1
+                For x As Integer = 0 To source.Width - 1
+                    red = source.GetPixel(x, y).R
+                    green = source.GetPixel(x, y).G
+                    blue = source.GetPixel(x, y).B
+
+                    rgb = red + green + blue
+
+                    If rgb < 128 Then
+                        source.SetPixel(x, y, Color.FromArgb(0, 0, 0))
+                    Else
+                        source.SetPixel(x, y, color.FromArgb(255, 255, 255))
+                    End If
+                Next
+            Next
+            PictureBox3.Image = source
+            PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+    End Sub
+
+    Private Sub Exit_Program(sender As Object, e As EventArgs) Handles Button4.Click
+        Application.Exit()
+    End Sub
+End Class
